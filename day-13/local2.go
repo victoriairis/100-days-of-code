9da2906da1469a34dc80a56958dfc1372288f037
Victoria Iris <victoria.iris@esig.com.br>
2020-12-30 10:08:15 -0300

    day 13, some go code

---
+++ day-13/local2.go
@@ -0,0 +1,23 @@
+
+package main
+
+import "fmt"
+
+func local(nome, cidade string) (regiao, estado string){
+	switch cidade{
+	case "Mossoró","Natal":
+		estado = "RN"
+
+	case "Aracaju":
+		estado = "SE"
+
+	default: 
+		estado = "DESCONHECIDO"
+	}
+	return
+}
+
+func main(){
+	regiao, estado := local("iris", "Mossoró")
+	fmt.Printf("%s mora em %s", regiao, estado)
+}
